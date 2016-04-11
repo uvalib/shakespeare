d7b97c835fccf491d3b659f0d81595a86df77884
Jack Kelly <jlk4p@eservices.virginia.edu>
2016-04-11 18:05:41 -0400

    UX-811 #comment make fixed footer so close always displays; initialize calendar picker so it appears

--- scripts/request-tour.js
+++ scripts/request-tour.js
@@ -1,5 +1,10 @@
 // Handles the request tour modal dialog
 $(document).ready(function(){
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 2 // Creates a dropdown of 15 years to control year
+    });
+
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal-trigger').leanModal({
     dismissible: true,
