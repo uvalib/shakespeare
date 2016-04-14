100712e6b4b560623ce54742bda9ce87dd3c9f63
Jack Kelly <jlk4p@eservices.virginia.edu>
2016-04-14 11:21:15 -0400

    UX-811 #comment move to form directory, add frontmatter for PHP script values, create production config file, try send request as modal button

--- scripts/request-tour.js
+++ scripts/request-tour.js
@@ -4,10 +4,10 @@
 
 // Handles the request tour modal dialog
 $(document).ready(function(){
-    $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 2 // Creates a dropdown of 15 years to control year
-    });
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 2 // Creates a dropdown of 15 years to control year
+  });
 
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal-trigger').leanModal({
@@ -15,6 +15,44 @@ $(document).ready(function(){
     opacity: .5,
     in_duration: 300,
     out_duration: 200
+  });
+
+  // When patron clicks send request modal button
+  $('.modal-action .send-request').on('click', function() {
+    var tourRequestForm = $('#tour-request-form');
+    if (! tourRequestForm[0].checkValidity()) {
+      $('<input type="submit">').hide().appendTo(tourRequestForm).click().remove();
+      tourRequestForm.find(':submit').click();
+    } else {
+      var name = document.getElementById("name").value;
+      var phone = document.getElementById("phone").value;
+      var email = document.getElementById("email").value;
+      var affiliation = document.getElementById("affiliation").value;
+      var groupsize = document.getElementById("groupsize").value;
+      var desiredtourdate = document.getElementById("desiredtourdate").value;
+      var specialaccommodations = document.getElementById("specialaccommodations").value;
+      var specificrequest = document.getElementById("specificrequest").value;
+      var dataString = 'name=' + name + '&phone=' + phone + '&email=' + email +
+        '&affiliation=' + affiliation + '&groupsize=' + groupsize +
+        '&desiredtourdate=' + desiredtourdate +
+        '&specialaccommodations=' + specialaccommodations +
+        '&specificrequest=' + specificrequest;
+      $.ajax({
+        type: "POST",
+        url: "{{site.tour_request_ajax_submission_url}}",
+        data: dataString,
+        dataType: 'json',
+        success: function(data) {
+          $("#name").val("");
+          $("#phone").val("");
+          $("#email").val("");
+          $("#affiliation").val("");
+          $("#groupsize").val("");
+          $("#desiredtourdate").val("");
+          $("#specialaccommodations").val("");
+        }
+      });
+    }
   });
 });
 
@@ -38,6 +76,13 @@ function submitTourRequest() {
     data: dataString,
     dataType: 'json',
     success: function(data) {
+      $("#name").val("");
+      $("#phone").val("");
+      $("#email").val("");
+      $("#affiliation").val("");
+      $("#groupsize").val("");
+      $("#desiredtourdate").val("");
+      $("#specialaccommodations").val("");
       $('.request-tour-form').replaceWith(data.confirmationMsg);
     }
   });
