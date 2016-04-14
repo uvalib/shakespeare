b37c4aeb0b1bb3e433363148e5e28e5f715c8db1
Jack Kelly <jlk4p@eservices.virginia.edu>
2016-04-14 11:40:42 -0400

    update function call code instead of selector click code for testing

--- scripts/request-tour.js
+++ scripts/request-tour.js
@@ -18,11 +18,49 @@ $(document).ready(function(){
   });
 
   // When patron clicks send request modal button
-  $('.modal-action .send-request').on('click', function() {
-    var tourRequestForm = $('#tour-request-form');
-    if (! tourRequestForm[0].checkValidity()) {
-      $('<input type="submit">').hide().appendTo(tourRequestForm).click().remove();
-    } else {
+/*  $('.modal-action .send-request').on('click', function() {
+    $('#tour-request-form').submit(function(evt) {
+      evt.preventDefault();
+      var $form = $(this);
+      if ($form.valid()) {
+        var name = document.getElementById("name").value;
+        var phone = document.getElementById("phone").value;
+        var email = document.getElementById("email").value;
+        var affiliation = document.getElementById("affiliation").value;
+        var groupsize = document.getElementById("groupsize").value;
+        var desiredtourdate = document.getElementById("desiredtourdate").value;
+        var specialaccommodations = document.getElementById("specialaccommodations").value;
+        var specificrequest = document.getElementById("specificrequest").value;
+        var dataString = 'name=' + name + '&phone=' + phone + '&email=' + email +
+          '&affiliation=' + affiliation + '&groupsize=' + groupsize +
+          '&desiredtourdate=' + desiredtourdate +
+          '&specialaccommodations=' + specialaccommodations +
+          '&specificrequest=' + specificrequest;
+        $.ajax({
+          type: "POST",
+          url: "{{site.tour_request_ajax_submission_url}}",
+          data: dataString,
+          dataType: 'json',
+          success: function(data) {
+            $("#name").val("");
+            $("#phone").val("");
+            $("#email").val("");
+            $("#affiliation").val("");
+            $("#groupsize").val("");
+            $("#desiredtourdate").val("");
+            $("#specialaccommodations").val("");
+          }
+        });
+      }
+    });
+  });*/
+});
+
+function submitTourRequest() {
+  $('#tour-request-form').submit(function(evt) {
+    evt.preventDefault();
+    var $form = $(this);
+    if ($form.valid()) {
       var name = document.getElementById("name").value;
       var phone = document.getElementById("phone").value;
       var email = document.getElementById("email").value;
@@ -53,10 +91,7 @@ $(document).ready(function(){
       });
     }
   });
-});
-
-function submitTourRequest() {
-  var name = document.getElementById("name").value;
+/*  var name = document.getElementById("name").value;
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
   var affiliation = document.getElementById("affiliation").value;
@@ -84,5 +119,5 @@ function submitTourRequest() {
       $("#specialaccommodations").val("");
       $('.request-tour-form').replaceWith(data.confirmationMsg);
     }
-  });
+  });*/
 }
