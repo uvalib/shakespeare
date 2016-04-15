0e9007c5772eb7559afbc87f848805e41294dd62
Jack Kelly <jlk4p@eservices.virginia.edu>
2016-04-15 13:03:54 -0400

    UX-811 #comment add onSet function to get calendar to close after date selected

--- scripts/request-tour.js
+++ scripts/request-tour.js
@@ -6,7 +6,14 @@
 $(document).ready(function(){
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 2 // Creates a dropdown of 15 years to control year
+    selectYears: 2, // Creates a dropdown of 15 years to control year
+    format: 'mm/dd/yyyy',
+    closeOnSelect: true,
+    onSet: function(arg) {
+      if ('select' in arg) { // prevent closing on selecting month/year
+        this.close();
+      }
+    }
   });
 
   // The "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
